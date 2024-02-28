c0caaa82404d99902b9c922e1c1b5ea314615783
linzy-vosena <vihendalinzi@gmail.com>
2024-02-28 14:58:12 +0300

    shell update

--- shell.h
+++ shell.h
@@ -1,22 +1,52 @@
-#ifndef _SHELL_H
-#define _SHELL_H
+#ifndef SHELL_H
+#define SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include "term_log.c"
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <errno.h>
 
-char *args[64];
-pid_t x;
-int status;
-// char line[1024];
+#define BUFFER_SIZE 1024
+#define BUFSIZE 64
+#define TOKEN_BUFSIZE 64
 
-#define PROMPT "user$"
-void term_prompt ();
-char term_log (const char *c);
+#define PATH_LENGTH 1024
+#ifndef PATH_MAX
+#define PATH_MAX 1024
+#endif
 
+extern char **environ;
+
+int set_exit_code(int);
+int get_and_clean_line(char *, size_t *);
+int exit_value(int);
+int create_child(char **, char *);
+void get_tokens(const char *, int *, char ***);
+void free_tokens(char ***, int *);
+void exit_shell(char *args[], char *, int *, char *, int);
+void tokenize_str(char *, char ***, int *);
+void tokenize_commands(char *, char ***, int *);
+void tokenize_string(char *, char ***, int *);
+char *find_command(const char *, char *);
+char *get_location(char *);
+char **split_line(char *);
+char *custom_strdup(char *);
+void builtin_commands(char **);
+void set_env(char **);
+void unset_env(char **);
+int chagedir(char **, char *);
+int handle_commands(char **, int *, char *, char *);
+int is_variable_to_exclude(const char *);
+void print_env(char **);
+int compare_strings(const void *, const void *);
 
 #endif
+
